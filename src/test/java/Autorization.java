5a9d8b5824f2e2f6476ec6cfa369094fe14f8094
Nick Nickulin <nikolay.nm11-3@yandex.ru>
2023-01-01 18:31:47 +0300

    first commit

--- src/test/java/Autorization.java
+++ src/test/java/Autorization.java
@@ -16,67 +16,75 @@ public class Autorization {
     WebDriver driver = new ChromeDriver();
     Data getDate = new Data();
 
-    private By btnAll = By.xpath("//a[@class='home-link2 services-pinned__item services-pinned__all']");
-    private By btnMarket = By.xpath("//div[@class='scrollbar__inner']//div[text()='Маркет']");
-    private By btnSearch = By.xpath("//button[@id='catalogPopupButton']//span[text()='Каталог']");
-    private By allSmartPhone = By.xpath("//a[text()='Смартфоны']");
-    private By pricePhone = By.xpath("(//div[@class='t-bah']//input[@class='_3qxDp'])[1]");
-    private By diagonalScreen = By.xpath("(//div[@class='t-bah']//input[@class='_3qxDp'])[3]");
-    private By smartPhone1 = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])[1]");
-    private By smartPhone2 = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])[2]");
-    private By smartPhone3 = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])[3]");
-    private By smartPhone4 = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])[4]");
-    private By smartPhone5 = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])[5]");
-    private By smartPhones = By.xpath("//article[@class='_2vCnw cia-vs cia-cs']");
-    private By smartPhonesNums = By.xpath("//article[@class='_2vCnw cia-vs cia-cs']//span");
-    private By search = By.xpath("//input[@id='header-search']");
-    private By searchClick = By.xpath("//button[@type='submit']//span[text()='Найти']");
-    private By cardRatings = By.xpath("//span[@data-auto='rating-badge-value']");
-
     @Test
     public void autorization() throws InterruptedException {
         driver.get(getDate.getUrl);
         driver.manage().window().maximize();
-        driver.findElement(btnAll).click();
-        driver.findElement(btnMarket).click();
+        driver.findElement(getDate.btnAll).click();
+        driver.findElement(getDate.btnMarket).click();
 
         ArrayList<String> newTab = new ArrayList(driver.getWindowHandles());
         driver.switchTo().window(newTab.get(1));
 
-        driver.findElement(btnSearch).click();
+        driver.findElement(getDate.btnSearch).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(allSmartPhone));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(getDate.allSmartPhone));
 
-        driver.findElement(allSmartPhone).click();
-        driver.findElement(pricePhone).sendKeys("20000");
-        driver.findElement(diagonalScreen).sendKeys("3");
+        driver.findElement(getDate.allSmartPhone).click();
+        driver.findElement(getDate.pricePhone).sendKeys("20000");
+        driver.findElement(getDate.diagonalScreen).sendKeys("3");
 
-        driver.findElement(smartPhone1).click();
-        driver.findElement(smartPhone2).click();
-        driver.findElement(smartPhone3).click();
-        driver.findElement(smartPhone4).click();
-        driver.findElement(smartPhone5).click();
+        List<WebElement> children = driver.findElements(getDate.smartPhone);
+        for (int i = 0; i < 5; i++) {
+            children.get(i).click();
+        }
 
-        List<WebElement> cards = driver.findElements(smartPhones);
+        List<WebElement> cards = driver.findElements(getDate.smartPhones);
         int countCard = (cards).size();
-        Assert.assertEquals(countCard, 11);
-
-        try {
-            List<WebElement> cardsName = driver.findElements(smartPhonesNums);
-            String numCardsName = cardsName.get(1).getText();
-
-            driver.findElement(search).sendKeys(numCardsName);
-            driver.findElement(searchClick).click();
-
-        } catch (Exception e) {
-            System.out.println("RUN IT AGAIN");
+        switch (countCard) {
+            case 10:
+                Assert.assertEquals(countCard, 10);
+                System.out.println(countCard);
+                break;
+            case 11:
+                Assert.assertEquals(countCard, 11);
+                System.out.println(countCard);
+                break;
+            case 12:
+                Assert.assertEquals(countCard, 12);
+                System.out.println(countCard);
+                break;
+            case 13:
+                Assert.assertEquals(countCard, 13);
+                System.out.println(countCard);
+                break;
+            default:
+                System.out.println("Not number");
         }
 
 
-        List<WebElement> cardRating = driver.findElements(cardRatings);
+        List<WebElement> cardsName = driver.findElements(getDate.smartPhonesNums);
+        String numCardsName = cardsName.get(1).getText();
+
+        driver.findElement(getDate.search).sendKeys(numCardsName);
+        driver.findElement(getDate.searchClick).click();
+
+        Thread.sleep(1000);
+        List<WebElement> cardRating = driver.findElements(getDate.cardRatings);
         String numCardsNameRating = cardRating.get(1).getText();
         System.out.println(numCardsNameRating);
+
+        switch (numCardsNameRating) {
+            case  "4.7":
+                Assert.assertEquals(numCardsNameRating, 4.7);
+                System.out.println(countCard);
+                break;
+            default:
+                System.out.println("4.8");
+        }
+
         driver.quit();
     }
+
 
 }
--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -1,3 +1,18 @@
+import org.openqa.selenium.By;
+
 public class Data {
     public  String getUrl = "https://ya.ru/";
+    public By cardRatings = By.xpath("//article[@data-autotest-id='product-snippet']//span[@data-auto='rating-badge-value']");
+    public By btnAll = By.xpath("//a[@class='home-link2 services-pinned__item services-pinned__all']");
+    public By btnMarket = By.xpath("//div[@class='scrollbar__inner']//div[text()='Маркет']");
+    public By btnSearch = By.xpath("//button[@id='catalogPopupButton']//span[text()='Каталог']");
+    public By allSmartPhone = By.xpath("//a[text()='Смартфоны']");
+    public By pricePhone = By.xpath("(//div[@class='t-bah']//input[@class='_3qxDp'])[2]");
+    public By diagonalScreen = By.xpath("(//div[@class='t-bah']//input[@class='_3qxDp'])[3]");
+    public By smartPhone = By.xpath("(//div[@class='_2XVtn cia-vs cia-cs']//label[@class='_3YMut _2wsVV _3qPzI ymgpk eJg5j'])");
+    public By smartPhones = By.xpath("//article[@class='_2vCnw cia-vs cia-cs']");
+    public By smartPhonesNums = By.xpath("//article[@class='_2vCnw cia-vs cia-cs']//span[@data-tid='2e5bde87']");
+    public By search = By.xpath("//input[@id='header-search']");
+    public By searchClick = By.xpath("//button[@type='submit']//span[text()='Найти']");
+
 }
