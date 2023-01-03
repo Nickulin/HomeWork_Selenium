2830f57f2df612e4422eaec268185ac248e5911f
Nick Nickulin <nikolay.nm11-3@yandex.ru>
2023-01-03 12:41:50 +0300

    commit this testNg

--- src/test/java/Autorization.java
+++ src/test/java/Autorization.java
@@ -1,6 +1,6 @@
 import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
